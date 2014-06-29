6ed685b574dd3d8a4c409384bf7ba2282df9b988
lucassagaria <lucassagaria@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-06-29 14:18:04 +0000

    Creación de la clase User, parseo de los datos del response del login  y mejora en el manejo de erorres.

---
+++ trunk/Codigo/whitePowderApp/whitePowder/src/com/example/whitepowder/User.java
@@ -0,0 +1,57 @@
+package com.example.whitepowder;
+
+import java.util.Date;
+
+//Singleton pattern
+
+public class User {
+
+	private static User instance = null;
+	
+	String username;
+	String password;
+	String token;
+	Date logedAt;
+	char userType;
+
+	private User() {};
+	
+	public static User getUserInstance(){
+		if(instance==null){
+			instance = new User();
+		};
+		return instance;
+	};
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public Date getLogedAt() {
+		return logedAt;
+	}
+	public void setLogedAt(Date logedAt) {
+		this.logedAt = logedAt;
+	}
+	public char getUserType() {
+		return userType;
+	}
+	public void setUserType(char userType) {
+		this.userType = userType;
+	}
+	
+}
