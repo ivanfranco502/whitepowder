2730b43ba21a6438f97c29873032bf8b121a1685
shoaquin <shoaquin@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-07-10 03:15:17 +0000

    Nuevo:     Registro de usuarios     Ultimo codigo del Sandbox     UTN BA - Proyecto Final 2014 - G422 - Firmas y Respuestas del Web Service.doc

--- trunk/Codigo/Web/src/src/Tavros/HelpUserBundle/Controller/RegistrationController.php
+++ trunk/Codigo/Web/src/src/Tavros/HelpUserBundle/Controller/RegistrationController.php
@@ -8,55 +8,63 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class RegistrationController extends BaseController {
 
     public function registerAction() {
-//        DEBEN LLEGAR LOS SIGUIENTES CAMPOS:
-//        fos_user_registration_form_username
-//        fos_user_registration_form_email
-//        fos_user_registration_form_plainPassword_first
-//        fos_user_registration_form_plainPassword_second
-//        fos_user_registration_form__token
-        $form = $this->container->get('fos_user.registration.form');
-        $form['username'] = $_POST('fos_user_registration_form_username');
-        $form['email'] = $_POST['fos_user_registration_form_email'];
-        $form['plainPassword']['first'] = $request->get('fos_user_registration_form_plainPassword_first');
-        $form['plainPassword']['second'] = $request->get('fos_user_registration_form_plainPassword_second');
-        $form['_token'] = $request->get('fos_user_registration_form__token');
+        
+        $logger = $this->container->get('logger');
+        
+        if ($this->container->get('request')->getMethod() == 'POST') {
+            $params = $_POST;
+        } else {
+            $response = new JsonResponse(array(
+                'status' => "ERROR",
+                'code' => 100,
+                'message' => "Método no permitido."));
+            return $response;
+        }
+        
+        $username = $params['username'];
+        $email = $params['email'];
+        $password = $params['password'];
+        $inactive = $params['inactive'];
+        $superadmin = $params['superadmin'];
+        $role = $params['role'];
 
-        $formHandler = $this->container->get('fos_user.registration.form.handler');
-        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+        $manipulator = $this->container->get('fos_user.util.user_manipulator');
+        $userManager = $this->container->get('fos_user.user_manager');
 
-        $process = $formHandler->process($confirmationEnabled);
-        if ($process) {
-            $user = $form->getData();
+        if ($userManager->findUserByUsername($username)) {
+            $response = new JsonResponse(array(
+                'status' => "ERROR",
+                'code' => 101,
+                'message' => "El nombre de usuario ya se encuentra registrado."));
+            return $response;
+        }
 
-            $authUser = false;
-            if ($confirmationEnabled) {
-                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
-                $route = 'fos_user_registration_check_email';
-            } else {
-                $authUser = true;
-                $route = 'fos_user_registration_confirmed';
-            }
+        if ($userManager->findUserByEmail($email)) {
+            $response = new JsonResponse(array(
+                'status' => "ERROR",
+                'code' => 102,
+                'message' => "El email ya se encuentra registrado."));
+            return $response;
+        }
 
-            $this->setFlash('fos_user_success', 'registration.flash.user_created');
-            $url = $this->container->get('router')->generate($route);
-            $response = new RedirectResponse($url);
-
-            if ($authUser) {
-                $this->authenticateUser($user, $response);
-            }
-
+        try {
+            $manipulator->create($username, $password, $email, !$inactive, $superadmin);
+            $manipulator->addRole($username, $role);
+        } catch (Exception $ex) {
+            $response = new JsonResponse(array(
+                'status' => "ERROR",
+                'code' => 103,
+                'message' => "No se pudo crear el usuario, vuelva a intentarlo."));
+            $logger->error('[TAVROS - ERROR]' . $ex);
             return $response;
         }
 
         $response = new JsonResponse(array(
-            'status' => "ERROR",
-            'code' => 1,
-            'message' => "All the information should be provided."));
+            'status' => "SUCCESS",
+            'code' => 001,
+            'message' => "El usuario fue creado con éxito."));
 
         return $response;
-//        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.' . $this->getEngine(), array(
-//                    'form' => $form->createView(),
-//        ));
     }
 
 }
--- trunk/Codigo/Web/src/vendor/friendsofsymfony/user-bundle/FOS/UserBundle/Controller/SecurityController.php
+++ trunk/Codigo/Web/src/vendor/friendsofsymfony/user-bundle/FOS/UserBundle/Controller/SecurityController.php
@@ -1,23 +1,15 @@
 <?php
 
-/*
- * This file is part of the FOSUserBundle package.
- *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+namespace Tavros\HelpUserBundle\Controller;
 
-namespace FOS\UserBundle\Controller;
-
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\Security\Core\SecurityContext;
 
-class SecurityController extends ContainerAware
-{
-    public function loginAction()
-    {
+class SecurityController extends BaseController {
+
+    public function loginAction() {
         $request = $this->container->get('request');
         /* @var $request \Symfony\Component\HttpFoundation\Request */
         $session = $request->getSession();
@@ -43,34 +35,10 @@ class SecurityController extends ContainerAware
         $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
 
         return $this->renderLogin(array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-            'csrf_token' => $csrfToken,
+                    'last_username' => $lastUsername,
+                    'error' => $error,
+                    'csrf_token' => $csrfToken,
         ));
     }
 
-    /**
-     * Renders the login template with the given parameters. Overwrite this function in
-     * an extended controller to provide additional data for the login template.
-     *
-     * @param array $data
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function renderLogin(array $data)
-    {
-        $template = sprintf('FOSUserBundle:Security:login.html.%s', $this->container->getParameter('fos_user.template.engine'));
-
-        return $this->container->get('templating')->renderResponse($template, $data);
-    }
-
-    public function checkAction()
-    {
-        throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
-    }
-
-    public function logoutAction()
-    {
-        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
-    }
 }
