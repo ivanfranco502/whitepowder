cf859733dfb30e417c44eff3b0d4f3d345a9c484
Lucas Sagaria <6941074+lucassagaria@users.noreply.github.com>
2014-07-21 03:00:46 +0000

    Primeras clases modulo de reconocimiento de pistas

--- Codigo/whitePowderApp/whitePowder/src/com/whitepowder/slope/recognizer/SlopeRecognizerActivity.java
+++ Codigo/whitePowderApp/whitePowder/src/com/whitepowder/slope/recognizer/SlopeRecognizerActivity.java
@@ -1,7 +1,16 @@
 package com.whitepowder.slope.recognizer;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SlopeRecognizerActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		SlopeDownloaderThread sdt = new SlopeDownloaderThread(this);
+		sdt.execute();
+	}
 
 }
