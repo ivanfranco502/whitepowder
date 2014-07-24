203a88be7160037bcfba3c7d0ec73da7b5fd3141
lucassagaria <lucassagaria@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-07-24 02:40:00 +0000

    Avance en SlopeRecognition

--- trunk/Codigo/whitePowderApp/whitePowder/src/com/whitepowder/slope/recognizer/SlopeContainer.java
+++ trunk/Codigo/whitePowderApp/whitePowder/src/com/whitepowder/slope/recognizer/SlopeContainer.java
@@ -1,8 +1,14 @@
 package com.whitepowder.slope.recognizer;
 
+import java.util.ArrayList;
+
+public class SlopeContainer {
+	int code;
+	ArrayList<SimplifiedSlope> payload;
+}
+
 class SimplifiedSlope {
 	int slop_id;
 	String slop_description;
 	int slop_recognized;
-	
 }
