72f3fc2af84306e1bdba7afa794a4fa9d86058b8
shoaquin <shoaquin@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-08-04 01:43:33 +0000

    Cambios base de datos API basic information

--- Codigo/Web/src/src/Tavros/InternalApiBundle/Controller/DefaultController.php
+++ Codigo/Web/src/src/Tavros/InternalApiBundle/Controller/DefaultController.php
@@ -3,12 +3,53 @@
 namespace Tavros\InternalApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Tavros\InternalApiBundle\Entity\ApiResponse;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use \FOS\UserBundle\Entity\User as User;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('TavrosInternalApiBundle:Default:index.html.twig', array('name' => $name));
+class DefaultController extends Controller {
+
+    public function basicInformationAction() {
+
+        $logger = $this->container->get('logger');
+        $serializer = $this->container->get('jms_serializer');
+        $apiResponse = new ApiResponse();
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $em = $this->container->get('Doctrine')->getManager();
+
+        if (!$this->container->get('request')->getMethod() == 'POST') {
+            $apiResponse->setCode(404);
+            $response->setContent($serializer->serialize($apiResponse, 'json'));
+            return $response;
+        }
+
+        $content = json_decode($this->container->get('request')->getContent());
+
+        $token = $em->getRepository('TavrosDomainBundle:Token')->findOneByToken($content->_token);
+
+        if (!$token) {
+            $apiResponse->setCode(110);
+            $response->setContent($serializer->serialize($apiResponse, 'json'));
+            return $response;
+        }
+
+        $generalInformation = $em->getRepository('TavrosDomainBundle:GeneralInformation')->findAll();
+
+        if (!$generalInformation) {
+            $apiResponse->setCode(116);
+            $response->setContent($serializer->serialize($apiResponse, 'json'));
+            return $response;
+        }
+
+        $apiResponse->setCode(200);
+        $apiResponse->setPayload($generalInformation[0]);
+        $response->setContent($serializer->serialize($apiResponse, 'json'));
+        return $response;
+
+        
     }
-    
+
 }
