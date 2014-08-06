20d3d8478d9fe8d1e4ea2be70311921e0c2d809d
lucassagaria <lucassagaria@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-08-06 00:39:08 +0000

    Refactor del nombre de la clase y variables

---
+++ trunk/Codigo/whitePowderApp/whitePowder/src/com/whitepowder/storage/AppStorage.java
@@ -0,0 +1,8 @@
+package com.whitepowder.storage;
+
+public class AppStorage{
+	
+	public static final String GENERAL_STORAGE_SHARED_PREFS = "simplified_slopes_container.file";	
+	public static final String SIMPLIFIED_SLOPES = "Simplified_Slopes";
+	
+}
