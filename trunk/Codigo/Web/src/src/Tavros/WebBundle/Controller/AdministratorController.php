24caa2cc4def2721861aa6498ee0cf567cc39f4f
shoaquin <shoaquin@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-08-14 03:16:28 +0000

    Adminitrador    home tilted    about.html.twig

--- trunk/Codigo/Web/src/src/Tavros/WebBundle/Controller/AdministratorController.php
+++ trunk/Codigo/Web/src/src/Tavros/WebBundle/Controller/AdministratorController.php
@@ -7,8 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class AdministratorController extends Controller {
 
     public function indexAction() {
-        
-        return $this->render('TavrosWebBundle:Administrator:index.html.twig');
+
+        return $this->render('TavrosWebBundle:Administrator:index.html.twig', Array('home_active' => 'active'));
+    }
+
+    public function aboutAction() {
+
+        return $this->render('TavrosWebBundle:Administrator:about.html.twig', Array('about_active' => 'active'));
     }
 
 }
