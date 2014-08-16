313675f0fe4b81b864307186d256884c60538b65
shoaquin <shoaquin@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-08-16 13:22:32 +0000

    Primer ejemplo de mapa en la web posición Hardcoded

--- trunk/Codigo/Web/src/web/bundles/tavrosweb/js/main.js
+++ trunk/Codigo/Web/src/web/bundles/tavrosweb/js/main.js
@@ -1,6 +1,16 @@
-$('.menu li a').click(function(e) {
-  var $this = $(this);
-  if (!$this.hasClass('active')) {
-    $this.addClass('active');
-  }
-});
+var map;
+function initialize() {
+    var mapOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(-41.183162, -71.449619),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    map = new google.maps.Map(document.getElementById('center-map'),
+            mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+$(document).ready(
+
+        );
