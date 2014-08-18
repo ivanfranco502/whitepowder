853b96e12061050bf13a8620c5d76b875e490a8b
shoaquin <shoaquin@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-08-18 22:26:10 +0000

    GeneralInformationAdmin only edit
    fix coordinates issue

--- trunk/Codigo/Web/src/web/bundles/tavrosweb/js/main.js
+++ trunk/Codigo/Web/src/web/bundles/tavrosweb/js/main.js
@@ -2,8 +2,8 @@ var map;
 function initialize() {
     var mapOptions = {
         zoom: 14,
-        center: new google.maps.LatLng(parseFloat($('#y_coordinate').text()), 
-                                        parseFloat($('#x_coordinate').text())),
+        center: new google.maps.LatLng(parseFloat($('#x_coordinate').text()),
+                parseFloat($('#y_coordinate').text())),
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     map = new google.maps.Map(document.getElementById('center-map'),
