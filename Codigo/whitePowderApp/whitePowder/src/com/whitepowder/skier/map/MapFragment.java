55a5aed95bcd53fd27137bb17b9d4a31f8aab4d8
ignaciojabad <ignaciojabad@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-08-19 00:03:26 +0000

    Map fragment doesn't crash anymore!

--- Codigo/whitePowderApp/whitePowder/src/com/whitepowder/skier/map/MapFragment.java
+++ Codigo/whitePowderApp/whitePowder/src/com/whitepowder/skier/map/MapFragment.java
@@ -4,16 +4,30 @@ import com.example.whitepowder.R;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MapFragment extends Fragment {
-
+	
+	private static View rootView;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.skier_fragment_map, container,false);
+		if(rootView != null){
+			ViewGroup parent = (ViewGroup) rootView.getParent();
+			if (parent != null){
+				parent.removeView(rootView);
+			}
+		}
 		
+		try{
+			rootView = inflater.inflate(R.layout.skier_fragment_map, container,false);
+		}
+		catch (InflateException e){
+			/* map is already there, just return view as it is */
+		}
 		return rootView;		
 		
 	}
