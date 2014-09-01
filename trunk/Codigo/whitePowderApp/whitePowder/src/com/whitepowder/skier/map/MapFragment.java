a128c0570d883d6bce33e49173e7f54d4a0894df
lucassagaria <lucassagaria@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-09-01 03:01:13 +0000

    Configuración de zoom, tipo de mapa y centrado del mapa del esquiador

--- trunk/Codigo/whitePowderApp/whitePowder/src/com/whitepowder/skier/map/MapFragment.java
+++ trunk/Codigo/whitePowderApp/whitePowder/src/com/whitepowder/skier/map/MapFragment.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
 import com.example.whitepowder.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -47,32 +49,40 @@ public class MapFragment extends Fragment {
 	}
 	
 	public void setupMap(){
+				
+		//Gets map
+		
 		mMap = ((com.google.android.gms.maps.MapFragment) getFragmentManager().findFragmentById(R.id.skier_map_fragment)).getMap();
 		mMap.setMyLocationEnabled(true);
-		drawSlopes();	
-	}
-	
-	@Override
-	public void onStart(){
-		super.onStart();
-
-	
-	}
-	
-	private void drawSlopes(){
+		mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		
+		//Find points
 		
 		Gson gson = new Gson();
 		String data = read_file(getActivity().getApplicationContext(), StorageConstants.DRAWABLE_SLOPES_FILE);	
 		DrawableSlopeContainer dsc = gson.fromJson(data, DrawableSlopeContainer.class);
 		
+		drawSlopes(dsc);	
+		
+		//Setups zoom and center
+        
+        CameraUpdate cam = CameraUpdateFactory.newLatLngZoom(determineCenter(dsc),13);
+        mMap.moveCamera(cam);
+		
+	}
+	
+	@Override
+	public void onStart(){
+		super.onStart();
+	
+	}
+	
+	private void drawSlopes(DrawableSlopeContainer dsc){
+	
 		if(dsc.code==200){
 			
 			for(DrawableSlope ds: dsc.payload){
 				if(ds.getSlope_coordinates()!=null){
-					
-					MarkerOptions mo = new MarkerOptions();
-					mo.position(new LatLng(20,20));
-					mMap.addMarker(mo);
 					
 					PolylineOptions plo = new PolylineOptions();
 				    plo.width(6);
@@ -83,14 +93,42 @@ public class MapFragment extends Fragment {
 					}
 					 
 					 mMap.addPolyline(plo);
-				}
-			}						
+				};
+			};
+			
+			MarkerOptions mo = new MarkerOptions();
+			mo.position(new LatLng(20,20));
+			mMap.addMarker(mo);
 		};
 		
 		
 	}
 	
-	public String read_file(Context context, String filename) {
+	private LatLng determineCenter(DrawableSlopeContainer dsc){
+		double sumatoriaX=0;
+		double sumatoriaY=0;
+		int count=0;
+		
+		if(dsc.code==200){
+			
+			for(DrawableSlope ds: dsc.payload){
+				if(ds.getSlope_coordinates()!=null){					
+					sumatoriaX += ds.getSlope_coordinates().get(0).x;
+					sumatoriaY += ds.getSlope_coordinates().get(0).y;
+					count ++;
+				};
+			};
+			
+			return new LatLng(sumatoriaX/count, sumatoriaY/count);
+			
+		}
+		else{
+			return new LatLng(0, 0);
+		}
+
+	}
+	
+	private String read_file(Context context, String filename) {
         try {
             FileInputStream fis = context.openFileInput(filename);
             InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
