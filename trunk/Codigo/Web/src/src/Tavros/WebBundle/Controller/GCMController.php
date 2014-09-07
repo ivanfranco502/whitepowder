23ae66274143390be9f602b0c5e368d5a0a8932f
ivanfranco502 <ivanfranco502@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-09-07 22:01:10 +0000

    - GCM Server

---
+++ trunk/Codigo/Web/src/src/Tavros/WebBundle/Controller/GCMController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tavros\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GCMController extends Controller {
+
+    public function sendNotificationAction() {
+        // include apiKey
+        $apiKey = $this->container->getParameter('api_key');
+        
+        $registration_ids = array("APA91bEx-tzPcQj-bfl3TF9t9FqPCOIlK8ad6TWBnm0i49ZFnjyXMrv_4nF5fqGpHA_mMwQMyN04uji7PcKZUbmdqBoaEIXsrmICsAxQZ5XiIEqRX-Sv_aIx3l3BYnY-OU6jhu6o1837LqavDEHD9gDMWyn0TB_qxOFHUapoXHGq4HJiDkG3XaM");
+        $message = array("id" => "1",
+                        "title" => "Avalancha",
+                        "body" => "Se pudrió todo");
+        
+        // Set POST variables
+        $url = 'https://android.googleapis.com/gcm/send';
+ 
+        $fields = array(
+            'registration_ids' => $registration_ids
+            ,
+            'data' => $message
+        );
+ 
+        $headers = array(
+            'Authorization: key=' . $apiKey,
+            'Content-Type: application/json'
+        );
+        // Open connection
+        $ch = curl_init();
+ 
+        // Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+ 
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ 
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+ 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+ 
+        // Execute post
+        $result = curl_exec($ch);
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }
+ 
+        // Close connection
+        curl_close($ch);
+        echo $result;
+        
+        return $this->render('TavrosWebBundle:Administrator:center.html.twig', array('coordinate_x' => 10,
+                        'coordinate_y' => 50));
+    }
+}
