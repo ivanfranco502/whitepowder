2f6825bc7fc24dfd01f90d7d0ede254c79c2020f
shoaquin <shoaquin@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-09-10 03:25:30 +0000

    Puntos en mapa administrador

--- Codigo/Web/src/web/bundles/tavrosweb/js/main.js
+++ Codigo/Web/src/web/bundles/tavrosweb/js/main.js
@@ -1,17 +1,50 @@
 var map;
+var skiers = [];
+var markers = [];
+var iterator = 0;
+$.ajax({
+    dataType: "json",
+    url: "http://whitetavros.com/Sandbox/web/app_dev.php/internalApi/skier/getAllPosition",
+    success: function(data, textStatus, jqXHR) {
+        data.payload.forEach(function(skier) {
+            skiers.push(new google.maps.LatLng(parseFloat(skier.coor_X), parseFloat(skier.coor_Y)));
+        });
+        drop();
+    }
+});
+
+
 function initialize() {
+    var centerLatlng = new google.maps.LatLng(parseFloat($('#x_coordinate').text()), parseFloat($('#y_coordinate').text()));
     var mapOptions = {
-        zoom: 14,
-        center: new google.maps.LatLng(parseFloat($('#x_coordinate').text()),
-                parseFloat($('#y_coordinate').text())),
+        zoom: 10,
+        center: centerLatlng,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     map = new google.maps.Map(document.getElementById('center-map'),
             mapOptions);
 }
 
+function drop() {
+    for (var i = 0; i < skiers.length; i++) {
+        setTimeout(function() {
+            addMarker();
+        }, i * 200);
+    }
+}
+
+function addMarker() {
+    markers.push(new google.maps.Marker({
+        position: skiers[iterator],
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    }));
+    iterator++;
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
-$(document).ready(
+$(document).ready(function() {
 
-        );
+});
