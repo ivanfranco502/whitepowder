d80827ba96be57a33bd19163589af3e193612e93
ignaciojabad <ignaciojabad@21076118-4f6c-42bb-a97f-306ddafd824d>
2014-11-05 02:17:07 +0000

    bugfix

--- Codigo/whitePowderApp/whitePowder/src/com/whitepowder/skier/normsAndSigns/SignActivity.java
+++ Codigo/whitePowderApp/whitePowder/src/com/whitepowder/skier/normsAndSigns/SignActivity.java
@@ -56,7 +56,7 @@ public class SignActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.sign, menu);
+		//getMenuInflater().inflate(R.menu.sign, menu);
 		return true;
 	}
 
